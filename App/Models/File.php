9707b2b67eef5c144d7221a712440934c5b8ee73
Daniel Barros <daniel.barros_99@hotmail.com>
2022-01-11 19:02:36 +0000

    added queries to file.php -> create(),delete(),getByParent(),get()

--- App/Models/File.php
+++ App/Models/File.php
@@ -9,12 +9,18 @@ class File extends \Core\Model
 
     public static function get($id)
     {
-
+        $db = static::db();
+        $stmt = $db->prepare("SELECT * FROM file WHERE id=?");
+        $stmt->execute([ $id ]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public static function getByParent($parentId)
     {
-
+        $db = static::db();
+        $stmt = $db->prepare("SELECT * FROM file WHERE parent_id=?");
+        $stmt->execute([ $parentId ]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     // return true se tem acesso
     /*public static function hasAccess($userId, $fileId)
@@ -34,12 +40,16 @@ class File extends \Core\Model
     public static function create($ownerId, $name, $size, $type, $state = 'NONE', $parentId = null)
 
     {
-        $db = static::
+        $db = static::db();
+        $stmt = $db->prepare("INSERT INTO file (owner_id, name, size, type, state, parent_id) VALUES (?, ?, ?, ?, ?, ?);");
+        $stmt->execute([ $ownerId, $name, $size, $type, $state, $parentId]);
     }
 
     public static function delete($id)
     {
-
+        $db = static::db();
+        $stmt = $db->prepare("DELETE FROM file WHERE id=?");
+        $stmt->execute([ $id ]);
     }
 
 }
