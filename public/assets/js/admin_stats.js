3821c35c2eed7db8b5a4a390bcf21caa7d0322f9
Daniel Barros <daniel.barros_99@hotmail.com>
2022-02-15 18:23:59 +0000

    Migration: profile & user_stats

--- public/assets/js/admin_stats.js
+++ public/assets/js/admin_stats.js
@@ -72,6 +72,42 @@ const chartMemory = new Chart(memoryDiv, {
     },
 });
 
+/*
+
+    User chart
+
+*/
+
+const diskDivUser = document.getElementById("chart_disk_user");
+const chartDiskUser = new Chart(diskDivUser, {
+    type: 'doughnut',
+    data: {
+        labels: ['Utilização de espaço', 'Livre'],
+        datasets: [{
+            data: [0, 0],
+            backgroundColor: [
+                'rgba(0, 150, 136, 1)',
+                'rgba(255, 255, 255, 1)'
+            ],
+            borderColor: [
+                'rgba(0,0,0,1)',
+                'rgba(0, 0, 0, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+
+/*
+
+    Chart Update
+
+*/
+
 async function refreshStats() {
     const result = await fetch(`/dashboard/admin/stats/api`);
     const response = await result.json();
@@ -84,6 +120,10 @@ async function refreshStats() {
 
     chartMemory.data.datasets[0].data = [response.memory.used, response.memory.total - response.memory.used];
     chartMemory.update();
+
+
+    chartDiskUser.data.datasets[0].data = [response.disk.used, response.disk.total - response.disk.used];
+    chartDiskUser.update();
 }
 
 setInterval(refreshStats, 1000);
